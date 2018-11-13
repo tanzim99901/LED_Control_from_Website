1cc39288d2bac4975842ce4d5e7ce8e009668de4
Syed Tanzim Mubarrat <tanzim.mubarrat99901@gmail.com>
2018-11-13 16:17:55 +0600

    Add files via upload

---
+++ LED_control_from_website/LED_control_from_website.ino
@@ -0,0 +1,78 @@
+#include "SoftwareSerial.h"
+#include <OneWire.h>
+#define R_switch 3  // stop switch
+#define F_switch 4  // start switch
+
+String ssid = "Zeus";
+
+String password = "google123456";
+
+SoftwareSerial esp(62, 63);// RX, TX
+
+String data;
+String on_off;
+
+String server = "tanzim99901.atwebpages.com"; // host website
+
+String get_html = "/value.html"; // html file from where the Switch state has to be read
+
+int whitepin = 23;
+int bluepin = 25;
+int redpin = 31; // this LED will be controlled from the website
+
+boolean connection = 0;
+
+String go;
+boolean play = 1;
+
+void setup() {
+  pinMode(R_switch, INPUT);
+  pinMode(F_switch, INPUT);
+  pinMode (whitepin, OUTPUT);
+  pinMode (bluepin, OUTPUT);
+  pinMode (redpin, OUTPUT);
+
+  digitalWrite(whitepin, LOW);
+  digitalWrite(bluepin, LOW);
+  digitalWrite(redpin, LOW);
+  on_off = "0";
+  Serial.begin(115200);
+  while (digitalRead(F_switch));
+  esp.begin(9600);
+  digitalWrite(redpin, HIGH);
+  delay(100);
+  digitalWrite(redpin, LOW);
+  reset();
+
+  connectWifi();
+  Serial.println("DONE CONNECTION!");
+}
+
+void loop () 
+{
+  if (!digitalRead(R_switch))
+  {
+    digitalWrite(redpin, LOW);
+    digitalWrite(bluepin, LOW);
+    digitalWrite(redpin, LOW);
+    reset();
+    play = 0;
+    Serial.println("STOP");
+  }
+  if (play)
+  {
+    httpget();
+    Serial.println("DATA READ");
+    delay(1000);
+    //httppost();
+    digitalWrite(whitepin, LOW);
+  }
+}
+
+
+
+
+
+
+
+
---
+++ LED_control_from_website/wifi.ino
@@ -0,0 +1,98 @@
+void httpget ()
+{
+
+  esp.println("AT+CIPSTART=\"TCP\",\"" + server + "\",80");//start a TCP connection.
+
+  if ( esp.find("OK")) {
+
+    Serial.println("TCP connection ready");
+
+  } delay(100);
+
+  String getRequest = "GET " + get_html + " HTTP/1.0\r\nHost:" + server + "\r\n\r\n\r\n\r\n\r\n";
+  String sendCmd = "AT+CIPSEND=";
+  sendCmd += getRequest.length();
+  sendCmd += "\r\n";
+  esp.println(sendCmd);
+  delay(100);
+
+  if (esp.find(">")) {
+    Serial.println("Sending..");
+    esp.print(getRequest);
+
+    if ( esp.find("SEND OK")) {
+      Serial.println("Packet sent");
+      String tmpResp, tmpFin;
+      while (esp.available()) 
+      {
+        tmpResp = esp.readString();
+      }
+      tmpFin = tmpResp.substring(241, tmpResp.length());
+      int closed = tmpFin.indexOf('C');
+      tmpFin = tmpResp.substring(241, 241 + closed);
+      Serial.println(tmpFin);
+      if (tmpFin.indexOf('1') != -1) 
+      {
+        digitalWrite(redpin, HIGH);
+        on_off = "1";
+      }
+      else if (tmpFin.indexOf('0') != -1) 
+      {
+        digitalWrite(redpin, LOW);
+        on_off = "0";
+      }
+      //Serial.println(tmpResp);
+      //Serial.println(tmpResp.length());
+      digitalWrite(whitepin, HIGH);
+      
+      // close the connection
+
+      esp.println("AT+CIPCLOSE");
+      delay(100);
+    }
+  }
+}
+
+
+//reset the esp8266 module
+
+void reset() {
+
+  esp.println("AT+RST\r\n");
+
+  delay(1000);
+
+  if (esp.find("OK") ) Serial.println("Module Reset");
+  delay(1000);
+  esp.println("AT+CWMODE=3\r\n");
+  if (esp.find("OK") ) Serial.println("Mode changed");
+  delay(1000);
+}
+
+//connect to your wifi network
+
+void connectWifi() {
+
+  String cmd = "AT+CWJAP=\"" + ssid + "\",\"" + password + "\"";
+
+  esp.println(cmd);
+
+  delay(4000);
+
+  if (esp.find("OK")) {
+
+    Serial.println("Connected!");
+    connection = 1;
+    digitalWrite(bluepin, HIGH);
+  }
+
+  else {
+    Serial.println("Cannot connect to wifi");
+    connection = 0;
+    digitalWrite(bluepin, LOW);
+  }
+  if (!connection)
+  {
+    connectWifi();
+  }
+}
