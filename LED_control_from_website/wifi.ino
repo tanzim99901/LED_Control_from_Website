0eeb5364b531b3cd5d7acf494a3d50e8080f4ad6
Syed Tanzim Mubarrat <tanzim.mubarrat99901@gmail.com>
2019-03-23 13:53:13 +0600

    Update wifi.ino

--- LED_control_from_website/wifi.ino
+++ LED_control_from_website/wifi.ino
@@ -1,7 +1,7 @@
 void httpget ()
 {
 
-  esp.println("AT+CIPSTART=\"TCP\",\"" + server + "\",80");//start a TCP connection.
+  esp.println("AT+CIPSTART=\"TCP\",\"" + server + "\",80");   //start a TCP connection.
 
   if ( esp.find("OK")) {
 
@@ -11,38 +11,46 @@ void httpget ()
 
   String getRequest = "GET " + get_html + " HTTP/1.0\r\nHost:" + server + "\r\n\r\n\r\n\r\n\r\n";
   String sendCmd = "AT+CIPSEND=";
+  
   sendCmd += getRequest.length();
   sendCmd += "\r\n";
-  esp.println(sendCmd);
+  esp.println(sendCmd);   // send the request for sending header
   delay(100);
 
   if (esp.find(">")) {
     Serial.println("Sending..");
-    esp.print(getRequest);
+    esp.print(getRequest);      // send the GET request
 
     if ( esp.find("SEND OK")) {
       Serial.println("Packet sent");
       String tmpResp, tmpFin;
+
+      // read the reply from the GET request
       while (esp.available()) 
       {
         tmpResp = esp.readString();
       }
+
+      // trimming the string 
       tmpFin = tmpResp.substring(241, tmpResp.length());
       int closed = tmpFin.indexOf('C');
       tmpFin = tmpResp.substring(241, 241 + closed);
       Serial.println(tmpFin);
+
+      // LED ON if "1" found
       if (tmpFin.indexOf('1') != -1) 
       {
         digitalWrite(redpin, HIGH);
         on_off = "1";
       }
+
+      // LED OFF if "0" found
       else if (tmpFin.indexOf('0') != -1) 
       {
         digitalWrite(redpin, LOW);
         on_off = "0";
       }
-      //Serial.println(tmpResp);
-      //Serial.println(tmpResp.length());
+      
       digitalWrite(whitepin, HIGH);
       
       // close the connection
@@ -64,6 +72,8 @@ void reset() {
 
   if (esp.find("OK") ) Serial.println("Module Reset");
   delay(1000);
+
+  // change the mode
   esp.println("AT+CWMODE=3\r\n");
   if (esp.find("OK") ) Serial.println("Mode changed");
   delay(1000);
