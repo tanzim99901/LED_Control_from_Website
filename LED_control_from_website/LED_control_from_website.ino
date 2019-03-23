c8b36b5449da6134e7ac82e672c7ef5ceb489bf7
Syed Tanzim Mubarrat <tanzim.mubarrat99901@gmail.com>
2019-03-23 13:53:03 +0600

    Update LED_control_from_website.ino

--- LED_control_from_website/LED_control_from_website.ino
+++ LED_control_from_website/LED_control_from_website.ino
@@ -1,13 +1,15 @@
+// OTHER FUNCTIONS ON THE wifi.ino FILE
+
 #include "SoftwareSerial.h"
-#include <OneWire.h>
+
 #define R_switch 3  // stop switch
 #define F_switch 4  // start switch
 
-String ssid = "Zeus";
+String ssid = "Zeus";     // your network SSID
 
-String password = "google123456";
+String password = "google123456";     // your network password
 
-SoftwareSerial esp(10, 11);// RX, TX
+SoftwareSerial esp(10, 11);// (RX of Arduino, TX of Arduino)
 
 String data;
 String on_off;
@@ -16,8 +18,8 @@ String server = "tanzim99901.atwebpages.com"; // host website
 
 String get_html = "/value.html"; // html file from where the Switch state has to be read
 
-int whitepin = 5;
-int bluepin = 6;
+int whitepin = 5;   // status indicator LED
+int bluepin = 6;    // status indicator LED
 int redpin = 7; // this LED will be controlled from the website
 
 boolean connection = 0;
@@ -35,33 +37,37 @@ void setup() {
   digitalWrite(whitepin, LOW);
   digitalWrite(bluepin, LOW);
   digitalWrite(redpin, LOW);
+  
   on_off = "0";
   Serial.begin(115200);
-  while (digitalRead(F_switch));
+  while (digitalRead(F_switch));    // WAIT FOR THE START SWITCH
+  
   esp.begin(9600);
   digitalWrite(redpin, HIGH);
   delay(100);
   digitalWrite(redpin, LOW);
-  reset();
+  reset();      // RESET MODULE
 
-  connectWifi();
+  connectWifi();    // CONNECT TO WIFI
   Serial.println("DONE CONNECTION!");
 }
 
-void loop () 
+void loop ()
 {
+  // STOP SWITCH
   if (!digitalRead(R_switch))
   {
-    digitalWrite(redpin, LOW);
+    digitalWrite(whitepin, LOW);
     digitalWrite(bluepin, LOW);
     digitalWrite(redpin, LOW);
     reset();
     play = 0;
     Serial.println("STOP");
   }
+  
   if (play)
   {
-    httpget();
+    httpget();    // MAIN FUNCTION
     Serial.println("DATA READ");
     delay(1000);
     //httppost();
